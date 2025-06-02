7777b5d8999dc2485c2c7839932917b7c4fd9e14
Lucas Rosa <contact.lucassr@gmail.com>
2025-06-02 20:09:01 -0300

    Adicionar função para ajustar item da lista

--- src/app/presentation/view/tab1/todo-list/todo-list.component.ts
+++ src/app/presentation/view/tab1/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, Input, Signal, signal, WritableSignal } from '@angular/core';
-import { IonGrid, IonCol, IonCheckbox, IonRow } from "@ionic/angular/standalone";
+import { IonGrid, IonCol, IonCheckbox, IonRow, CheckboxChangeEventDetail } from "@ionic/angular/standalone";
 import { Todo } from 'src/app/structure/todo/domain/entity/todo';
 
 @Component({
@@ -17,11 +17,23 @@ export class TodoListComponent {
   @Input() todoList: WritableSignal<Array<Todo>> = signal([]);
 
   doneTodoList: Signal<Todo[]> = computed(() => this.todoList().filter(todo => todo.done));
-  
   pendingTodoList: Signal<Todo[]> = computed(() => this.todoList().filter(todo => ! todo.done));
   
   constructor() { }
 
- 
+  toggleTodo(isDone: boolean, todoId: string) {
+    this.todoList.update((todos: Array<Todo>) => {
+      return todos.map((todo: Todo) => {
+        if (todo.id === todoId) {
+          return { ...todo, done: isDone };
+        } else {
+          return todo;
+        }
+      });
+    });
+  }
 
+  handleCheckboxChange(isCheck: CheckboxChangeEventDetail<boolean>, todoID: string) {
+    this.toggleTodo(isCheck.checked, todoID);
+  }
 }
